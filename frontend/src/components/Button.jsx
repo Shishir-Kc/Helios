4e0718d05c833c806298117dc8bf030fc6475a4d
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-13 13:38:20 +0545

    Add Button component with primary/secondary/ghost/tertiary variants

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+export const Button = ({ variant = 'primary', children, className = '', ...props }) => {
+  return (
+    <button className={`btn btn-${variant} ${className}`} {...props}>
+      {children}
+    </button>
+  );
+};
